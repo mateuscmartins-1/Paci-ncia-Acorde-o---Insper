fe0762fa81482c68ec6bea9ac96bd362567ed7e3
Felipe S. Spina <felipess4@al.insper.edu.br>
2021-04-27 20:36:03 -0300

    Movimentos possíveis

--- codigo.py
+++ codigo.py
@@ -27,3 +27,27 @@ def extrai_valor(string):
         return string[0]
     else:
         return string[0]+string[1]
+#Lista movimentos possíveis no Paciência Acordeão
+def extrai_naipe(string):
+    if len(string)==2:
+        return string[1]
+    else:
+        return string[2]
+def extrai_valor(string):
+    if len(string)==2:
+        return string[0]
+    else:
+        return string[0]+string[1]
+def lista_movimentos_possiveis(baralho,indice):
+    movimentos_possiveis=[]
+    if indice>=1:
+        if extrai_naipe(baralho[indice])==extrai_naipe(baralho[indice-1]):
+            movimentos_possiveis.append(1)
+        elif extrai_valor(baralho[indice])==extrai_valor(baralho[indice-1]):
+            movimentos_possiveis.append(1)
+    if indice>=3:
+        if extrai_naipe(baralho[indice])==extrai_naipe(baralho[indice-3]):
+            movimentos_possiveis.append(3)
+        elif extrai_valor(baralho[indice])==extrai_valor(baralho[indice-3]):
+            movimentos_possiveis.append(3)
+    return movimentos_possiveis
