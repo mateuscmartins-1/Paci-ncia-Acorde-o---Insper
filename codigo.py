c4d7f9ce54550ba013f46f9e494b90ff3478b3f8
Mateus Martins <mateuscm2@al.insper.edu.br>
2021-04-27 19:27:08 -0300

    Atualização da primeira funcao do baralho

--- codigo.py
+++ codigo.py
@@ -1,3 +1,14 @@
 def cria_baralho():
-    lista = ['A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣']
-    return lista
+    naipes = ["♠", "♥", "♦", "♣"]
+    valor_carta = ["2","3","4","5","6","7","8","9","10","J","Q", "K", "A"]
+    baralho = []
+    i = 0
+    j = 0
+    while i < len(valor_carta) and j < len(naipes):
+        carta = "{}{}".format(valor_carta[i],naipes[j])
+        baralho.append(carta)
+        i+= 1
+        if i == 13:
+            j += 1
+            i = 0
+    return baralho
